b8aac069c9a0f480ffb27d1830abe0291b845a5a
VasiliiSokolov <vasilysokolov@mail.ru>
2022-12-05 21:44:46 +0300

    Добавьте файлы проекта.

---
+++ FileWritingTask2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <ctime>
+
+int main()
+{
+    std::srand(std::time(nullptr));
+    int width, height;
+    std::ofstream pic("pic.txt", std::ios::out);
+    std::cout << "Enter width and height of the picture!" << std::endl;
+    std::cin >> width >> height;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            pic << rand()%2;
+        }
+        pic << std::endl;
+    }
+    pic.close();
+}
+
